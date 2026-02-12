c1495489d8c022adc6fef9ae7bed457c2a2fb3b4
lpego <luca.pegoraro90@gmail.com>
2026-02-12 10:11:56 +0100

    Successful workflow, requires auth every run though

--- tests/testAuth.py
+++ tests/testAuth.py
@@ -2,8 +2,10 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 import io
 from googleapiclient.http import MediaIoBaseUpload
+from datetime import datetime
 
 OAUTH_CLIENT_JSON = "D:\\hardware-db\\OAuth_client-WSL_laptop.json"
+PARENT_DIR = "1UBiv4UnuLzDrOJbOgcRzgwqN2Y4Gv75S" # hardware-db Forms folder
 
 SCOPES = ["https://www.googleapis.com/auth/drive.file"]
 
@@ -17,6 +19,8 @@ drive_service = build("drive", "v3", credentials=creds)
 file_content = "Hello world!"
 media = MediaIoBaseUpload(io.BytesIO(file_content.encode("utf-8")), mimetype="text/plain")
 
+
+### Create text file for testing
 file = drive_service.files().create(
     body={"name": "test.txt"},
     media_body=media,
@@ -24,3 +28,32 @@ file = drive_service.files().create(
 ).execute()
 
 print("Created file ID:", file["id"])
+
+
+### Rename and move file to specified folder
+drive_service.files().update(
+    fileId=file["id"],
+    body={
+        "name": "Test File - "
+                f"{datetime.today().strftime('%Y-%m-%d_%H:%M:%S')}"
+    }
+).execute()
+
+# Get current parents
+file = drive_service.files().get(
+    fileId=file["id"],
+    fields="id, parents"
+).execute()
+
+previous_parents = ",".join(file.get("parents"))
+
+# Move the form
+drive_service.files().update(
+    fileId=file["id"],
+    addParents=PARENT_DIR,
+    removeParents=previous_parents,
+    fields="id, parents"
+).execute()
+
+print("File renamed and moved successfully:")
+print(f"https://docs.google.com/forms/d/{file["id"]}/edit")

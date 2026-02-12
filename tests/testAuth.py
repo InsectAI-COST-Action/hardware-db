37a8c4cc8b9298956c5dae491216c63dbcf5cf61
lpego <luca.pegoraro90@gmail.com>
2026-02-12 10:23:54 +0100

    Aligning with other scripts auth workflows

--- tests/testAuth.py
+++ tests/testAuth.py
@@ -1,18 +1,32 @@
 from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
-import io
+import io, os
 from googleapiclient.http import MediaIoBaseUpload
 from datetime import datetime
 
 OAUTH_CLIENT_JSON = "D:\\hardware-db\\OAuth_client-WSL_laptop.json"
+TOKEN_FILE = "token_testAuth.json"
 PARENT_DIR = "1UBiv4UnuLzDrOJbOgcRzgwqN2Y4Gv75S" # hardware-db Forms folder
 
 SCOPES = ["https://www.googleapis.com/auth/drive.file"]
 
-flow = InstalledAppFlow.from_client_secrets_file(
-    OAUTH_CLIENT_JSON, SCOPES
-)
-creds = flow.run_local_server(port=0)
+
+#### Authentication flow
+creds = None
+
+if os.path.exists(TOKEN_FILE):
+    creds = Credentials.from_authorized_user_file(TOKEN_FILE, SCOPES)
+
+if not creds or not creds.valid:
+    flow = InstalledAppFlow.from_client_secrets_file(
+        OAUTH_CLIENT_JSON, SCOPES
+    )
+    creds = flow.run_local_server(port=0)
+
+    with open(TOKEN_FILE, "w") as token:
+        token.write(creds.to_json())
+
 
 drive_service = build("drive", "v3", credentials=creds)
 
